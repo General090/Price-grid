855353551944762f83abf0df5efb41878d54f90f
Gospel Onuoha <gospelkalu33@gmail.com>
2024-07-05 14:23:22 +0100

    price-grid

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    screens: {
+      'sm': '320px',
+      // => @media (min-width: 320px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1440px',
+      // => @media (min-width: 1440px) { ... }
+
+    },
+    extend: {},
+  },
+  plugins: [],
+}
